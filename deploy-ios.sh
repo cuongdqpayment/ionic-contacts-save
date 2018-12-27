b3b8ae520596314d9dc9eb335479205c978bdb19
cuongdq.payment <cuongdq.payment@gmail.com>
2018-12-27 17:14:46 +0700

    cuongdq push by sh

--- deploy-ios.sh
+++ deploy-ios.sh
@@ -2,7 +2,7 @@
 #npm i
 ionic cordova platform remove ios
 ionic cordova platform add ios --save
-#ionic cordova build ios --prod --release
+ionic cordova build ios --prod --release
 #Open xcode select <project_name>.xcodeproj 
 #File --> Project settings --> Build system = Legacy Build System
 #project navigator -> select <project name> --> targets --> select <projectname> 
